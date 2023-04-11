8f10aea4626e2e455d51593ca1045f4225ac8866
matsuoshi <matsuoshi@gmail.com>
2023-04-11 11:23:15 +0900

    add heroImage to schema

--- src/content/_schemas.ts
+++ src/content/_schemas.ts
@@ -3,13 +3,17 @@ import { z } from "astro:content";
 export const blogSchema = z
   .object({
     title: z.string(),
-    date: z.string().or(z.date()).transform((val) => new Date(val)),
+    date: z
+      .string()
+      .or(z.date())
+      .transform(val => new Date(val)),
     tags: z.array(z.string()).default(["others"]),
-    description: z.string().optional().default(''),
+    description: z.string().optional().default(""),
     author: z.string().optional(),
     postSlug: z.string().optional(),
     draft: z.boolean().optional(),
-    image: z.string().optional()
+    image: z.string().optional(),
+    heroImage: z.boolean().optional().default(true),
   })
   .strict();
 
